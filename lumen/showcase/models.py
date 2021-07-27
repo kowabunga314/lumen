5a9f356a031bf44cbf4c7f57f2cb8718162a8a4b
kowabunga314 <dougfrost314@gmail.com>
2021-07-26 21:42:35 -0500

    created models and corresponding migration

--- lumen/showcase/models.py
+++ lumen/showcase/models.py
@@ -1,3 +1,18 @@
+from datetime import datetime
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+
+
+class Series(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Photo(models.Model):
+    name = models.CharField(max_length=128)
+    url = models.CharField(max_length=256)
+    location = models.CharField(max_length=128)
+    pub_date = models.DateField('Date of Publication', default=datetime.now)
+    series = ForeignKey(Series, on_delete=models.CASCADE)
+
