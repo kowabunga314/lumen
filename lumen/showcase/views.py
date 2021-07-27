ab5fdf9aeb6f26acc7cfe6c9144701e8bf0aa1fd
kowabunga314 <dougfrost314@gmail.com>
2021-07-27 17:55:09 -0500

    testing first pass at homepage layout

--- lumen/showcase/views.py
+++ lumen/showcase/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from showcase.models import Series
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is Saturn Lab's photography showcase")
+    series_list = Series.objects.order_by('name')[:5]
+    template = loader.get_template('showcase/index.html')
+    context = {
+        'series_list': series_list
+    }
+    return HttpResponse(template.render(context, request))
